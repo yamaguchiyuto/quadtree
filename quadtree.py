982e826e9e2fdd5222833a54d522d5e7399bac08
Yuto Yamaguchi <yamaguchiyuto@ddhcp219054.d.aist.go.jp>
2016-09-05 15:24:07 +0900

    Delete center attribute, rename __iter__ to gen_leaves(), add gen_areas(), let all areas have ids

--- quadtree.py
+++ quadtree.py
@@ -5,12 +5,26 @@ class Quadtree:
         self.leaves = {}
         self.root = self.divide(self.init_area(data,x1,y1,x2,y2),maxpoints,maxdivision)
 
-    def __iter__(self):
+    def gen_leaves(self):
         for aid in self.leaves:
             yield self.leaves[aid]
 
+    def gen_areas(self):
+        r = self.root
+        areas = [r]
+        level = 0
+        while len(areas) > 0:
+            new_areas = []
+            for a in areas:
+                yield (level,a)
+                for c in a.children:
+                    if c != None: new_areas.append(c)
+            areas = new_areas
+            level += 1
+
     def init_area(self,data,x1,y1,x2,y2):
-        initial = Area(x1,y1,x2,y2)
+        initial = Area(self.sequential_id,x1,y1,x2,y2)
+        self.sequential_id += 1
         for d in data:
             initial.append(d)
         return initial
@@ -28,7 +42,8 @@ class Quadtree:
                 0 2
                 1 3
                 """
-                sub_area = Area(area.x1+dx*xl, area.y1+dy*yl, area.x1+(1+dx)*xl, area.y1+(1+dy)*yl)
+                sub_area = Area(self.sequential_id,area.x1+dx*xl, area.y1+dy*yl, area.x1+(1+dx)*xl, area.y1+(1+dy)*yl)
+                self.sequential_id += 1
                 division.append(sub_area)
 
         """ Assign points to new areas """
@@ -43,11 +58,8 @@ class Quadtree:
     def divide(self, area, maxpoints, division_left):
         if division_left == 0 or area.number_of_points() <= maxpoints:
             """ Terminate if the number of points in this area does not exceed `maxpoints` """
-            area.set_fixed(self.sequential_id)
-            area.calc_center()
-            self.leaves[self.sequential_id] = area
-            self.sequential_id += 1
-            return area
+            area.set_fixed()
+            self.leaves[area.aid] = area
         else:
             """ Do division if the number of points in this aera exceeds `maxpoints` """
             next_level = self.subdivide(area)
@@ -56,13 +68,14 @@ class Quadtree:
                 child = self.divide(next_level[i],maxpoints,division_left-1)
                 area.set_child(i,child)
             """ Return divided area """
-            return area
+        return area
 
-    def get_area_id(self,p):
-        return self.root.covered(p)
+    def get_area_id(self,p,depth=-1):
+        return self.root.covered(p,depth)
 
 class Area:
-    def __init__(self,x1,y1,x2,y2):
+    def __init__(self,aid,x1,y1,x2,y2):
+        self.aid = aid
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
@@ -74,17 +87,6 @@ class Area:
         1 3
         """
         self.children = [None,None,None,None]
-
-    def calc_center(self):
-        if self.number_of_points() == 0:
-            self.center = (self.x1 + (self.x2-self.x1)/2, self.y1 + (self.y2-self.y1)/2)
-        else:
-            sumx = 0.
-            sumy = 0.
-            for p in self.points_:
-                sumx += p[0]
-                sumy += p[1]
-            self.center = (sumx/len(self.points_), sumy/len(self.points_))
 
     def append(self, p):
         self.points_.append(p)
@@ -98,24 +100,27 @@ class Area:
     def is_fixed(self):
         return self.fixed
 
-    def set_fixed(self,aid):
+    def set_fixed(self):
         self.fixed = True
-        self.aid = aid
 
     def set_child(self,n,area):
         self.children[n] = area
 
-    def covered(self,p):
+    def covered(self,p,depth):
         if self.cover(p):
+            if depth==0: return self
             if self.fixed:
-                return self.aid
+                return self
             else:
                 cid = 0
                 if self.x1 + (self.x2 - self.x1) / 2 < p[0]:
                     cid += 2
                 if self.y1 + (self.y2 - self.y1) / 2 < p[1]:
                     cid += 1
-                return self.children[cid].covered(p)
+                if depth == -1:
+                    return self.children[cid].covered(p,depth)
+                else:
+                    return self.children[cid].covered(p,depth-1)
         else:
             return None
 
@@ -139,7 +144,7 @@ if __name__ == '__main__':
         return data
 
     def usage(com):
-        print "[USAGE]: python %s [x left] [y up] [x right] [y down] [maxpoints] [maxdivision] [data filepath]" % com
+        print "[USAGE]: python %s [x left] [y up] [x right] [y down] [maxpoints] [maxdivision] [data filepath] (output filepath)" % com
 
     if len(sys.argv) < 8:
         usage(sys.argv[0])
@@ -155,11 +160,24 @@ if __name__ == '__main__':
 
     qtree = Quadtree(data,x1,y1,x2,y2,maxpoints,maxdivision)
 
-    print "AreaID,x_left,y_up,x_right,y_down,x_center,y_center"
-    for a in qtree:
-        print "%s,%s,%s,%s,%s,%s,%s" % (a.aid,a.x1,a.y1,a.x2,a.y2,a.center[0],a.center[1])
+    if len(sys.argv) == 9:
+        import pickle
+        output_filepath = sys.argv[8]
+        with open(output_filepath, 'w') as f:
+            pickle.dump(qtree, f)
+
+    print "AreaID,Depth,x_left,y_up,x_right,y_down"
+    for level,a in qtree.gen_areas():
+        print "%s,%s,%s,%s,%s,%s" % (a.aid,level,a.x1,a.y1,a.x2,a.y2)
 
     p = (0.37,0.55)
     print 
     print "Query: (%s,%s)" % p
-    print "Returned Area ID: %s" % qtree.get_area_id(p)
+    area = qtree.get_area_id(p,0)
+    print "Level 0 - Returned Area: %s (%s,%s) - (%s,%s)" % (area.aid, area.x1,area.y1,area.x2,area.y2)
+    area = qtree.get_area_id(p,1)
+    print "Level 1 - Returned Area: %s (%s,%s) - (%s,%s)" % (area.aid, area.x1,area.y1,area.x2,area.y2)
+    area = qtree.get_area_id(p,2)
+    print "Level 2 - Returned Area: %s (%s,%s) - (%s,%s)" % (area.aid, area.x1,area.y1,area.x2,area.y2)
+    area = qtree.get_area_id(p,3)
+    print "Level 3 - Returned Area: %s (%s,%s) - (%s,%s)" % (area.aid, area.x1,area.y1,area.x2,area.y2)
